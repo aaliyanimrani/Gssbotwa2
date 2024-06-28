97470b0a2418655698654143ab786570d6b4a82a
aaliyanimrani <irfanimrani321@gmail.com>
2024-06-28 09:45:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://chat.whatsapp.com/I4AqHAd8Lmf2zVrpZjRtAy'
+global.linkGroup = 'https://chat.whatsapp.com/I4AqHAd8Lmf2zVrpZjRtAy'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "Aaliyan imrani";
